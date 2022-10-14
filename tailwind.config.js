41f23963f46d9a2633c7c71260621f9f05e56665
GreattitJY <great.tit.jaejae@gmail.com>
2022-10-14 21:27:44 +0900

    before:disabled 클래스 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,17 @@
 // const { default: scrollbarSize } = require('scrollbar-size');
 
 module.exports = {
-  content: ["./index.html"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    screens: {
-      'md' : [
-        {'max' : '780px' }
-      ]
+    content: ["./index.html"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        screens: {
+            md: [{ max: "780px" }],
+        },
+        extend: {
+            fontFamily: {
+                Sans: ["Noto Sans KR", "sans-serif"],
+            },
+        },
     },
-    extend: {
-      fontFamily: {
-      Sans:  ["Noto Sans KR", "sans-serif"]
-      },
-    },
-  },
-  plugins: [
-    require('tailwind-scrollbar')({ nocompatible: true }),
-  ],
-}
+    plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
+};
