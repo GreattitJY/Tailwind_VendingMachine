74ad8c9fbb460310d81adb3251a064a057c579fc
GreattitJY <great.tit.jaejae@gmail.com>
2022-10-26 23:27:57 +0900

    음료 클릭 시 리스트 추가 구현중

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,45 @@
-// 입금 및 거스름돈 반환
+const goods = document.querySelector("#section-goods");
+const goodsList = goods.querySelectorAll("#section-goods button");
 const purchase = document.querySelector("#section-purchase");
 const inpMoney = purchase.querySelector("#input-money input");
 const inpBtn = purchase.querySelector("#input-button");
+const balance = purchase.querySelector("#balance");
+const balBtn = purchase.querySelector("#balBtn");
 const wallet = document.querySelector("#wallet");
-const balance = document.querySelector("#balance");
+const basket = document.querySelector("#section-basket");
+const basketList = basket.querySelector("#section-basket ul");
 
+// goods
+const addBasket = function () {
+    console.log(this === goodsList[0]);
+    // if (this === goodsList[0] && basketList)
+    const aa = document.querySelector("#test");
+    if (document.querySelector("#test") === null) {
+        const productLi = document.createElement("li");
+        const productBtn = document.createElement("button");
+        const productImg = document.createElement("img");
+        const productName = document.createElement("strong");
+        const productPrice = document.createElement("span");
+        basketList.appendChild(productLi);
+        productLi.appendChild(productBtn);
+        productBtn.appendChild(productImg);
+        productBtn.appendChild(productName);
+        productBtn.appendChild(productPrice);
+        productLi.id = "test";
+    }
+};
+
+Array.prototype.forEach.call(goodsList, (item) => {
+    item.addEventListener("click", addBasket);
+});
+
+// purchase\
 const toInteger = function (string) {
     return string.match(/[0-9]/g).join("") | 0;
 };
-const comma = function (num) {
-    return num
+
+const comma = function (money) {
+    return money
         .toString()
         .split("")
         .reverse()
@@ -18,28 +48,36 @@ const comma = function (num) {
         .join("");
 };
 
-let balMoney = 0;
+let leftMoney = 0;
 
 const putMoney = function () {
     const myMoney = toInteger(wallet.textContent);
+    if (inpMoney.value.length === 0) {
+        alert("금액을 넣어주세요.");
+        return;
+    }
     if (inpMoney.value > myMoney || inpMoney.value < 0) {
         if (inpMoney.value > myMoney) {
             alert("소지금이 부족합니다.");
         } else {
             alert("알맞은 금액을 넣어주세요.");
         }
-        return;
+        return (inpMoney.value = "");
     }
     const curMoney = comma(myMoney - inpMoney.value);
     wallet.textContent = curMoney + " 원";
-    balMoney += parseInt(inpMoney.value);
-    balance.textContent = balMoney + "원";
+    leftMoney += parseInt(inpMoney.value);
+    balance.textContent = comma(leftMoney) + " 원";
+    inpMoney.value = "";
 };
+
 inpBtn.addEventListener("click", putMoney);
 
-// Todo :: 거스름돈 반환 이벤트 달기
-
-// 잔액 확인용 (추후 지우기)
-const checkBalMoney = function () {
-    return balMoney;
+const returnMoney = function () {
+    const curMoney = toInteger(wallet.textContent) + leftMoney;
+    leftMoney = 0;
+    balance.textContent = 0 + " 원";
+    wallet.textContent = comma(curMoney) + " 원";
 };
+
+balBtn.addEventListener("click", returnMoney);
